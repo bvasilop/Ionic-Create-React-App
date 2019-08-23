b36bc602e814ebe04ea552274bdd302da0919aa3
bvasilop <bvasilop@gmail.com>
2019-08-22 20:41:52 -0700

    added project files for Ionic-Create-React-App

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import '@ionic/core/css/core.css';
+import '@ionic/core/css/ionic.bundle.css';
+import {
+  IonApp,
+  IonContent,
+  IonCard,
+  IonCardHeader,
+  IonCardTitle,
+  IonCardSubtitle,
+  IonButton,
+  IonCheckbox,
+  IonList,
+  IonItem,
+  IonLabel
+} from '@ionic/react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const form = [
+  { val: 'Pepperoni', isChecked: true },
+  { val: 'Sausage', isChecked: false },
+  { val: 'Mushroom', isChecked: false }
+];
+
+class App extends Component {
+  render() {
+    return (
+      <IonApp>
+        <IonContent>
+          <IonCard>
+            <IonCardHeader>
+              <IonCardSubtitle>Welcome to Ionic</IonCardSubtitle>
+              <IonCardTitle>Running on React</IonCardTitle>
+            </IonCardHeader>
+            <IonButton onClick={this.props.dismissModal}>Cancel</IonButton>
+
+          </IonCard>
+            {/*-- Default Checkbox --*/}
+    <IonCheckbox />
+
+{/*-- Disabled Checkbox --*/}
+<IonCheckbox disabled={true} />
+
+{/*-- Checked Checkbox --*/}
+<IonCheckbox checked={true} />
+
+{/*-- Checkbox Colors --*/}
+<IonCheckbox color="primary" />
+<IonCheckbox color="secondary" />
+<IonCheckbox color="danger" />
+<IonCheckbox color="light" />
+<IonCheckbox color="dark" />
+
+{/*-- Checkboxes in a List --*/}
+<IonList>
+  { form.map(({val, isChecked}) => (
+    <IonItem key={val}>
+      <IonLabel>{val}</IonLabel>
+      <IonCheckbox slot="end" value={val} checked={isChecked} />
+    </IonItem>
+  )) }
+</IonList>
+        </IonContent>
+      </IonApp>
+    );
+  }
 }
 
 export default App;
